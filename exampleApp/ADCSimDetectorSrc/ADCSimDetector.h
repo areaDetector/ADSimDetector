29bbe5b2305908721550e859b7004e9437e3b6d7
Mark Rivers <rivers@cars.uchicago.edu>
2016-02-28 11:17:51 -0600

    Removed writeFlaot64

--- exampleApp/ADCSimDetectorSrc/ADCSimDetector.h
+++ exampleApp/ADCSimDetectorSrc/ADCSimDetector.h
@@ -25,7 +25,6 @@ public:
 
     /* These are the methods that we override from asynNDArrayDriver */
     virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
-    virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
     virtual void report(FILE *fp, int details);
     void simTask(); /**< Should be private, but gets called from C, so must be public */
 
