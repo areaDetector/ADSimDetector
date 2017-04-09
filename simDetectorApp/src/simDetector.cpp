a77cee99c0b7efb21eece8ef0ad663e0f4908c14
Mark Rivers <rivers@cars.uchicago.edu>
2017-04-09 11:00:00 -0500

    Don't release lock when calling doCallbacksGenericPointer

--- simDetectorApp/src/simDetector.cpp
+++ simDetectorApp/src/simDetector.cpp
@@ -798,13 +798,9 @@ void simDetector::simTask()
 
         if (arrayCallbacks) {
           /* Call the NDArray callback */
-          /* Must release the lock here, or we can get into a deadlock, because we can
-           * block on the plugin lock, and the plugin can be calling us */
-          this->unlock();
           asynPrint(this->pasynUserSelf, ASYN_TRACE_FLOW,
                     "%s:%s: calling imageData callback\n", driverName, functionName);
           doCallbacksGenericPointer(pImage, NDArrayData, 0);
-          this->lock();
         }
 
         /* See if acquisition is done */
