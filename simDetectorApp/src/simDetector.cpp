2bfbea4a2132317ef6ae6270dbf169caa21cdd33
James Souter <james.souter@diamond.ac.uk>
2023-11-08 10:32:21 +0000

    Base image time stamp off epicsTS not startTime

--- simDetectorApp/src/simDetector.cpp
+++ simDetectorApp/src/simDetector.cpp
@@ -822,8 +822,8 @@ void simDetector::simTask()
 
         /* Put the frame number and time stamp into the buffer */
         pImage->uniqueId = imageCounter;
-        pImage->timeStamp = startTime.secPastEpoch + startTime.nsec / 1.e9;
         updateTimeStamp(&pImage->epicsTS);
+        pImage->timeStamp = pImage->epicsTS.secPastEpoch + pImage->epicsTS.nsec/1e9;
 
         /* Get any attributes that have been defined for this driver */
         this->getAttributes(pImage->pAttributeList);
